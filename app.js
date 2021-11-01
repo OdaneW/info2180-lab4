3b68b91e37cccc7dd9ac1c862e480459900a54ce
Odane Williams <odanew1@gmail.com>
2021-10-31 23:56:22 -0500

    Modified styles.css, index.html

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ window.onload = function(){
     
     
     const search = document.getElementsByTagName('button')
-    search[0].addEventListener("click", function(){
+    search[0].addEventListener("click", function(event){
         var httpRequest = new XMLHttpRequest();
        
         httpRequest.onreadystatechange = function(){
             if (this.readyState == XMLHttpRequest.DONE && this.status == 200){
-                alert(this.responseText)
+                document.getElementById('result').innerHTML = this.responseText;
             }
         };
         httpRequest.open("GET", "superheroes.php",true);
         httpRequest.send();
+        let item = document.getElementById('search').value;
         
-        
+    event.preventDefault()   
     });
-
-    
+    preventDefault()
 }
 /*
 
